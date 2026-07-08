ab520e3d533a664dfe475b4e8c5913ca623d3dd8
Constantinos Vafias <cv2134@nyu.edu>
2026-07-07 22:15:31 -0400

    Text Mesh Pro / Card Labels and dragging

--- Speedy&Tranca/Assets/Scripts/Card.cs
+++ Speedy&Tranca/Assets/Scripts/Card.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro; 
 public class Card : MonoBehaviour
 {
     // Attributes for the card class
+    public TMP_Text cardLabel;
     public enum Suit { Spades, Clubs, Hearts, Diamonds}
     public enum Rank { Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13, Ace = 14 }
     
     public Suit suit;
     public Rank rank;
+
+    [HideInInspector] public Player owner;
+
+    private bool isDragging;
+    private Vector3 dragOffset;
+    private float dragZ;
+    public bool IsDragging => isDragging;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,5 +34,45 @@ public class Card : MonoBehaviour
     {
         suit = newSuit;
         rank = newRank;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        if (cardLabel != null)
+        {
+            cardLabel.text = $"{rank}\n{suit}";
+            cardLabel.color = Color.black; 
+        }
+    }
+    
+    void OnMouseDown()
+    {
+        isDragging = true;
+        dragZ = Camera.main.WorldToScreenPoint(transform.position).z;
+        dragOffset = transform.position - GetMouseWorldPos();
+
+        GetComponent<SpriteRenderer>().sortingOrder = 100; // bring to front while held
+        owner?.OnCardDragStart(this);
+    }
+
+    void OnMouseDrag()
+    {
+        if (!isDragging) return;
+        transform.position = GetMouseWorldPos() + dragOffset;
+    }
+
+    void OnMouseUp()
+    {
+        if (!isDragging) return;
+        isDragging = false;
+        owner?.OnCardDragEnd(this);
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = dragZ;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 }
--- Speedy&Tranca/Assets/Scripts/Player.cs
+++ Speedy&Tranca/Assets/Scripts/Player.cs
@@ -16,6 +16,7 @@ public class Player : MonoBehaviour
     public GameObject cardPrefab;
     private List<Card> hand = new List<Card>();
     private int numCards;
+    public Transform handContainer;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,7 +32,7 @@ public class Player : MonoBehaviour
             generateHand();
             for (int i = 0; i < numCards; i++)
             {
-                Debug.Log(hand[i]);
+                Debug.Log($"Card: {hand[i].rank} of {hand[i].suit}");
             }
         }
     }
@@ -40,8 +41,69 @@ public class Player : MonoBehaviour
     {
         for (int i = 0; i < 11; i++)
         {
-            hand.Add(deck.Deal());
+            Card dealt = deck.Deal();
+            dealt.owner = this;          // NEW: so the card can notify us
+            hand.Add(dealt);
         }
         numCards = hand.Count;
+        DisplayHand();
+    }
+
+    void DisplayHand()
+    {
+        foreach (Card card in hand)
+            card.gameObject.SetActive(false);
+
+        int count = hand.Count;
+        float fanAngle = 30f;
+        float radius = 15f;
+        // Push the arc center further down so cards fan upward from bottom center
+        Vector3 arcCenter = handContainer.position + new Vector3(0, -radius + 2f, 0);
+
+        for (int i = 0; i < count; i++)
+        {
+            Card card = hand[i];
+            card.gameObject.SetActive(true);
+
+            if (card.IsDragging) continue;
+            
+            float t = count > 1 ? i / (float)(count - 1) : 0.5f;
+            float angle = Mathf.Lerp(fanAngle / 2, -fanAngle / 2, t);
+
+            float radian = angle * Mathf.Deg2Rad;
+            Vector3 offset = new Vector3(Mathf.Sin(radian), Mathf.Cos(radian), 0) * radius;
+            card.transform.position = arcCenter + offset;
+
+            card.transform.rotation = Quaternion.Euler(0, 0, angle);
+            card.GetComponent<SpriteRenderer>().sortingOrder = i;
+        }
+    }
+
+    public void RemoveCard(Card card)
+    {
+        card.gameObject.SetActive(false);
+        hand.Remove(card);
+        DisplayHand();
+    }
+
+    public void AddCard(Card card)
+    {
+        hand.Add(card);
+        DisplayHand();
+        card.owner = this;
+    }
+    
+    // NEW: drag hooks called by Card
+    public void OnCardDragStart(Card card)
+    {
+        // Hook for later: highlight a play zone, disable other input, etc.
+    }
+
+    public void OnCardDragEnd(Card card)
+    {
+        // For now: snap back into hand formation.
+        // Later, check a drop zone here before calling DisplayHand()
+        // to support actually "playing" the card instead of returning it.
+        DisplayHand();
     }
 }
